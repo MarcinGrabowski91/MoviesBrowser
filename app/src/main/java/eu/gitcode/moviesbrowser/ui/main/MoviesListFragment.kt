c5a0cf4cf61c4e7e5923920477bf5b8cde4b7cf7
MarcinGrabowski91 <marcingrabowski91@gmail.com>
2020-12-02 17:40:01 +0100

    Initial commit

---
+++ app/src/main/java/eu/gitcode/moviesbrowser/ui/main/MoviesListFragment.kt
@@ -0,0 +1,30 @@
+package eu.gitcode.moviesbrowser.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import eu.gitcode.moviesbrowser.R
+
+class MoviesListFragment : Fragment() {
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.movies_list_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+    companion object {
+        fun newInstance() = MoviesListFragment()
+    }
+}
