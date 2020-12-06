34f228a9d0f1bf80bf18e35e53725113fd68e816
MarcinGrabowski91 <marcingrabowski91@gmail.com>
2020-12-06 14:22:28 +0100

    Movie fragment added

--- app/src/main/java/eu/gitcode/moviesbrowser/movies/presentation/MoviesListFragment.kt
+++ app/src/main/java/eu/gitcode/moviesbrowser/movies/presentation/MoviesListFragment.kt
@@ -3,7 +3,10 @@ package eu.gitcode.moviesbrowser.movies.presentation
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
 import eu.gitcode.moviesbrowser.R
+import eu.gitcode.moviesbrowser.movies.domain.enum.MovieType
+import eu.gitcode.moviesbrowser.movies.domain.model.MovieDomainModel
 import kotlinx.android.synthetic.main.movies_list_fragment.*
 import org.koin.android.ext.android.inject
 
@@ -14,15 +17,46 @@ class MoviesListFragment : Fragment(R.layout.movies_list_fragment),
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        moviesRecyclerView.adapter = moviesAdapter
+        moviesRecyclerView.apply {
+            adapter = moviesAdapter
+            addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
+        }
+        mockData() // TODO: 06/12/2020
     }
 
     override fun onMovieClicked(id: Long) {
-        TODO("Not yet implemented")
+        parentFragmentManager.beginTransaction()
+            .replace(R.id.container, MovieFragment.newInstance())
+            .addToBackStack(null)
+            .commit()
     }
 
     override fun onShowClicked(id: Long) {
-        TODO("Not yet implemented")
+        parentFragmentManager.beginTransaction()
+            .replace(R.id.container, MovieFragment.newInstance())
+            .addToBackStack(null)
+            .commit()
+    }
+
+    private fun mockData() {
+        moviesAdapter.setItems(
+            listOf(
+                MovieDomainModel(
+                    1,
+                    MovieType.MOVIE,
+                    "Lion King",
+                    1993,
+                    1500
+                ),
+                MovieDomainModel(
+                    3,
+                    MovieType.SHOW,
+                    "Matrix",
+                    2000,
+                    11111
+                )
+            )
+        )
     }
 
     companion object {
