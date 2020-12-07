9447f99c2a4581d0bcd5bf728c8d204cf2ad8975
MarcinGrabowski91 <marcingrabowski91@gmail.com>
2020-12-07 06:09:14 +0100

    Required API interfaces added

---
+++ app/src/main/java/eu/gitcode/moviesbrowser/films/data/api/MovieApi.kt
@@ -0,0 +1,22 @@
+package eu.gitcode.moviesbrowser.films.data.api
+
+import eu.gitcode.moviesbrowser.films.data.model.MovieDataModel
+import eu.gitcode.moviesbrowser.films.data.model.MovieDetailsDataModel
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface MovieApi {
+    @GET("movies/trending")
+    suspend fun getTrendingMovies(
+        @Query("years") years: String = "",
+        @Query("limit") page: Int = DEFAULT_PAGE_SIZE
+    ): MovieDataModel
+
+    @GET("movies/{id}")
+    suspend fun getMovieSummary(@Path("id") id: Long): MovieDetailsDataModel
+
+    companion object {
+        private const val DEFAULT_PAGE_SIZE = 100
+    }
+}
---
+++ app/src/main/java/eu/gitcode/moviesbrowser/films/data/api/ShowApi.kt
@@ -0,0 +1,22 @@
+package eu.gitcode.moviesbrowser.films.data.api
+
+import eu.gitcode.moviesbrowser.films.data.model.ShowDataModel
+import eu.gitcode.moviesbrowser.films.data.model.ShowDetailsDataModel
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface ShowApi {
+    @GET("shows/trending")
+    suspend fun getTrendingShows(
+        @Query("years") years: String = "",
+        @Query("limit") page: Int = DEFAULT_PAGE_SIZE
+    ): ShowDataModel
+
+    @GET("shows/{id}")
+    suspend fun getShowSummary(@Path("id") id: Long): ShowDetailsDataModel
+
+    companion object {
+        private const val DEFAULT_PAGE_SIZE = 100
+    }
+}
